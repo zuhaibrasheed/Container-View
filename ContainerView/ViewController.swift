7f21b622f022e2365efbb06d07ab7b7dc5fc301e
Zuhaib Rasheed <zuhaib.rasheed@icloud.com>
2017-03-25 15:03:16 +0500

    Initial Commit

---
+++ ContainerView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContainerView
+//
+//  Created by Zuhaib Rasheed on 25/03/2017.
+//  Copyright © 2017 Lifo Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
